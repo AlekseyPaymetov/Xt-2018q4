030d9992a3b5af4c06b0f9aa6eabb34ff2c3c15b
AlekseyPaymetov <fa_l@mail.ru>
2018-11-22 02:47:41 +0300

    Add solution for task 1.2. Triangle

---
+++ Task1_1/Epam.Task1_1/2_Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number of strings (N>0): ");
+            int.TryParse(Console.ReadLine(), out int n);
+
+            for (int i=1; i<=n; i++)
+            {
+                for (int j=1; j<=i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
