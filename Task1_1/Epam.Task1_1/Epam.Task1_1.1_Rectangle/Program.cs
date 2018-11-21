2c1c4ef67ba92c7c0e488cdd8a8aa99766f21af4
AlekseyPaymetov <fa_l@mail.ru>
2018-11-22 02:36:54 +0300

    Add solution for task 1.1. Rectangle

---
+++ Task1_1/Epam.Task1_1/Epam.Task1_1.1_Rectangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1_1._1_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a: ");
+            int.TryParse(Console.ReadLine(), out int a);
+            if (a < 1)
+            {
+                Console.WriteLine("Input incorrect: a must be bigger then 0");
+                return;
+            }
+            Console.Write("Please input b: ");
+            int.TryParse(Console.ReadLine(), out int b);
+            if (b < 1)
+            {
+                Console.WriteLine("Input incorrect: b must be bigger then 0");
+                return;
+            }
+
+            Console.WriteLine("Square of rectagle: {0}",a*b);
+        }
+    }
+}
