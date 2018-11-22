8456a8a4ca3492e1b192cc4d97b7a50578ec130f
AlekseyPaymetov <fa_l@mail.ru>
2018-11-22 18:52:15 +0300

    Add solution for task 1.4. X-mas tree and 1.5 Sum of numbers

---
+++ Task1_1/Epam.Task1_1/Epam,Task1_1.4 X-mas tree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam_Task1_1._4_X_mas_tree
+{
+    class Program
+    {
+
+        static void DrawAnotherTriangle(int n)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number of strings (N>0): ");
+            int.TryParse(Console.ReadLine(), out int n);
+            if (n < 1)
+            {
+                Console.WriteLine("Incorrect input. N must be bigger then 0");
+                return;
+            }
+
+            int maxLength = 1 + 2 * (n - 1);
+  
+            for (int k = 1; k <= n; k++)
+            {
+
+                int startPosition = maxLength / 2 + 1;
+                int currentLenght = 1;
+                for (int i = 1; i <= k; i++, currentLenght += 2, startPosition--)
+                {
+                    for (int j = 1; j <= maxLength; j++)
+                    {
+                        if (j < startPosition || j >= startPosition + currentLenght)
+                        {
+                            Console.Write(' ');
+                        }
+                        else
+                        {
+                            Console.Write('*');
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ Task1_1/Epam.Task1_1/Epam.Task1_1.5 Sum of Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1_1._5_Sum_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum=0;
+            const int number = 1000;
+
+            //for (int i = 0, j = 0; j < number; i += 5, j += 3)
+            //{
+            //    if (i>=number)
+            //    {
+            //        sum += j;
+            //    }
+            //    else
+            //    {
+            //        sum += i + j;
+            //    }
+            //}
+
+            // It's solution better than upper?
+            for (int i = 0; i < number; i += 5)
+            {
+                sum += i;
+            }
+
+            for (int i = 0; i < number; i += 3)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"the sum of all numbers less than {number} times 3 and 5: {sum}");
+        }
+    }
+}
