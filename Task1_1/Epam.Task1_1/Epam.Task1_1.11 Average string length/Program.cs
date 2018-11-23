683b59e8afc111274c68b1c0c9368c812ada491d
AlekseyPaymetov <fa_l@mail.ru>
2018-11-23 16:43:15 +0300

    Add solution for tast 1.11 Avarage string length

---
+++ Task1_1/Epam.Task1_1/Epam.Task1_1.11 Average string length/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1_1._11_Average_string_length
+{
+
+    class Program
+    {
+
+        static bool IsSymbol(char c) =>
+            (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string:");
+            string s=Console.ReadLine();
+            char[] arrayOfChars = s.ToArray();
+
+            bool wordStarted = false;
+            int countOfWords = 0;
+            int countOfSymblosInWords = 0;
+            
+            for (int i=0; i<arrayOfChars.Length; i++)
+            {
+                
+                if (IsSymbol(arrayOfChars[i]))
+                {
+                    countOfSymblosInWords++;
+                    wordStarted = true;
+                }
+                else
+                {
+                    if (wordStarted)
+                    {
+                        wordStarted = false;
+                        countOfWords++;
+                    } 
+                }
+            }
+
+            if (IsSymbol(arrayOfChars[arrayOfChars.Length-1]))
+            {
+                countOfWords++;
+            }
+            Console.WriteLine("Avarage length of words is: {0:0.###}",(double)countOfSymblosInWords/(countOfWords));
+
+        }
+    }
+}
