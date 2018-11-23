ab19db4ee2159a98a8972f19cafc1770f48735f9
AlekseyPaymetov <fa_l@mail.ru>
2018-11-23 11:42:26 +0300

    Add solution for task 1.9 Non-Negative sum

---
+++ Task1_1/Epam.Task1_1/Epam.Task1_1.9 Non-negative sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1_1._9_Non_negative_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            int[] array = new int[n];
+            Random rng = new Random();
+
+            Console.WriteLine("Initial array: ");
+            for (int i=0; i<array.Length; i++)
+            {
+                array[i] = rng.Next(-10, 10);
+                Console.Write($"{array[i]} ");
+            }
+
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0) sum += array[i];
+            }
+            Console.WriteLine($"{Environment.NewLine}Sum of non-negative elements: {sum}");
+        }
+    }
+}
