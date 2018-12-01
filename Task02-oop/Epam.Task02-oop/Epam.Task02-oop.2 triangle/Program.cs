297da60e33dabf62597eb2698dbdf5e849a5343c
AlekseyPaymetov <fa_l@mail.ru>
2018-12-01 20:09:55 +0300

    Added solution for task 2.2 triangle

---
+++ Task02-oop/Epam.Task02-oop/Epam.Task02-oop.2 triangle/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02_oop._2_triangle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please input the first side of the triangle a: ");
+            string input = Console.ReadLine();
+            if (!double.TryParse(input, out double a))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+            Console.Write("Please input the second side of the triangle b: ");
+            input = Console.ReadLine();
+            if (!double.TryParse(input, out double b))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+            Console.Write("Please input the third side of the triangle c: ");
+            input = Console.ReadLine();
+            if (!double.TryParse(input, out double c))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+            try
+            {
+                Triangle triangle = new Triangle(a, b, c);
+                Console.WriteLine($"{Environment.NewLine}Perimeter of input triangle: {triangle.Perimeter}");
+                Console.WriteLine("Area of input triangle: {0:.000}", triangle.Area);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Task02-oop/Epam.Task02-oop/Epam.Task02-oop.2 triangle/Triangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02_oop._2_triangle
+{
+    public class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle(double a, double b, double c)
+        {
+            this.IsCorrect(a, b, c);
+
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double Perimeter
+        {
+            get => this.a + this.b + this.c;
+        }
+
+        public double Area
+        {
+            get => Math.Sqrt((this.Perimeter / 2) * ((this.Perimeter / 2) - this.a) * ((this.Perimeter / 2) - this.b) * ((this.Perimeter / 2) - this.c));
+        }
+
+        private void IsCorrect(double a, double b, double c)
+        {
+            if (a <= 0)
+            {
+                throw new ArgumentException("a should be positive", nameof(this.a));
+            }
+
+            if (b <= 0)
+            {
+                throw new ArgumentException("b should be positive", nameof(this.b));
+            }
+
+            if (c <= 0)
+            {
+                throw new ArgumentException("c should be positive", nameof(this.c));
+            }
+
+            if (a >= b + c || b >= a + c || c >= a + b)
+            {
+                throw new ArgumentException("Entered sides do not form a triangle");
+            }
+        }
+    }
+}
