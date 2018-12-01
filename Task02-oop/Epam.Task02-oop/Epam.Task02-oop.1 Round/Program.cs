b15465232abbc1bd7eac300afb33aa3e28585d9e
AlekseyPaymetov <fa_l@mail.ru>
2018-12-01 12:00:26 +0300

    Added solution for task 2.1 Round

---
+++ Task02-oop/Epam.Task02-oop/Epam.Task02-oop.1 Round/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02_oop._1_Round
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string inputLine;
+
+            Console.Write("Please enter x: ");
+            inputLine = Console.ReadLine();
+            if (!double.TryParse(inputLine, out double x))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+            Console.Write("Please enter y: ");
+            inputLine = Console.ReadLine();
+            if (!double.TryParse(inputLine, out double y))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+            Console.Write("Please enter radius: ");
+            inputLine = Console.ReadLine();
+            if (!double.TryParse(inputLine, out double radius))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+
+            try
+            {
+                Round someRound = new Round(x, y, radius);
+
+                Console.WriteLine("Length of input round: {0:.000}", someRound.Length);
+                Console.WriteLine("Area of input round: {0:.000}", someRound.Area);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Task02-oop/Epam.Task02-oop/Epam.Task02-oop.1 Round/Round.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02_oop._1_Round
+{
+    public class Round
+    {
+        private double x;
+        private double y;
+        private double radius;
+
+        public Round(double x, double y, double r)
+        {
+            this.x = x;
+            this.y = y;
+
+            if (r <= 0)
+            {
+                throw new ArgumentException("Radius should be positive", nameof(this.radius));
+            }
+
+            this.radius = r;
+        }
+
+        public double Length
+        {
+            get => 2 * Math.PI * this.radius;
+        }
+
+        public double Area
+        {
+            get => Math.PI * this.radius * this.radius;
+        }
+    }
+}
