0c62959dc4a00564e94dfb0c514b47f9159aaa55
AlekseyPaymetov <fa_l@mail.ru>
2018-12-02 22:08:45 +0300

    Added solution for task 2.4 my string

---
+++ Task02-oop/Epam.Task02-oop/Epam.Task02-oop.4 my string/MyString.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02_oop._4_my_string
+{
+    public class MyString
+    {
+        private char[] myString;
+
+        public MyString(string inputString)
+        {
+            this.myString = inputString.ToCharArray();
+        }
+
+        public int Length
+        {
+            get => this.myString.Length;
+        }
+
+        public char this[int index]
+        {
+            get
+            {
+                this.CheckIndex(index);
+                return this.myString[index];
+            }
+
+            set
+            {
+                this.CheckIndex(index);
+                this.myString[index] = value;
+            }
+        }
+
+        public static bool operator >(MyString string1, MyString string2)
+        {
+            if (string1.Length > string2.Length)
+            {
+                for (int i = 0; i < string2.Length; i++)
+                {
+                    if (string1[i] > string2[i])
+                    {
+                        return true;
+                    }
+
+                    if (string1[i] < string2[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < string1.Length; i++)
+                {
+                    if (string1[i] > string2[i])
+                    {
+                        return true;
+                    }
+
+                    if (string1[i] < string2[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        public static bool operator <(MyString string1, MyString string2) =>
+             string2 > string1 && string1 != string2;
+
+        public static bool operator >=(MyString string1, MyString string2) =>
+            string1 > string2 || string1 == string2;
+
+        public static bool operator <=(MyString string1, MyString string2) =>
+            string1 < string2 || string1 == string2;
+
+        public static MyString operator +(MyString string1, MyString string2)
+        {
+            return new MyString(string1.ToString() + string2.ToString());
+        }
+
+        public static bool operator ==(MyString string1, MyString string2)
+        {
+            if (string1.Length != string2.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < string1.Length; i++)
+                {
+                    if (string1[i] != string2[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public static bool operator !=(MyString string1, MyString string2) =>
+            !(string1 == string2);
+
+        public override string ToString() =>
+            new string(this.myString);
+
+        public int IndexOf(char symbol)
+        {
+            for (int i = 0; i < this.myString.Length; i++)
+            {
+                if (this.myString[i] == symbol)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        // Or i must write full serch function (use for)?
+        public int IndexOf(string subString) =>
+            new string(this.myString).IndexOf(subString);
+
+        public int IndexOf(MyString mySubString) =>
+             new string(this.myString).IndexOf(mySubString.ToString());
+
+        public char[] ToCharArray() =>
+            this.myString;
+        
+        private void CheckIndex(int index)
+        {
+            if (index < 0 || index > this.myString.Length)
+            {
+                throw new ArgumentException("Wrong index");
+            }
+        }
+    }
+}
---
+++ Task02-oop/Epam.Task02-oop/Epam.Task02-oop.4 my string/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02_oop._4_my_string
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please input some string1: ");
+            string string1 = Console.ReadLine();
+            Console.Write("Please input some string2: ");
+            string string2 = Console.ReadLine();
+
+            MyString myString1 = new MyString(string1);
+            MyString myString2 = new MyString(string2);
+
+            Console.WriteLine("string1 as string: {0}", myString1.ToString());
+            Console.WriteLine($"string concatenation: {myString1 + myString2}");
+            if (myString1 > myString2)
+            {
+                Console.WriteLine("string1 > string2");
+            }
+
+            if (myString1 < myString2)
+            {
+                Console.WriteLine("string1 < string2");
+            }
+
+            if (myString1 == myString2)
+            {
+                Console.WriteLine("string1 == string2");
+            }
+            else
+            {
+                Console.WriteLine("string1 != string2");
+            }
+
+            if (myString1 >= myString2)
+            {
+                Console.WriteLine("string1 >= string2");
+            }
+
+            if (myString1 <= myString2)
+            {
+                Console.WriteLine("string1 <= string2");
+            }
+
+            if (myString1.IndexOf('a') >= 0)
+            {
+                Console.WriteLine($"String1 has symbol 'a'. It's index is: {myString1.IndexOf('a')}.");
+            }
+            else
+            {
+                Console.WriteLine($"String1 has not symbol 'a'.");
+            }
+
+            if (myString1.IndexOf(myString2) >= 0)
+            {
+                Console.WriteLine("String1 has string2 as substring.");
+            }
+            else
+            {
+                Console.WriteLine("String1 has not string2 as substring.");
+            }
+        }
+    }
+}
