57b83dc7c7bbbf7b96f0cd880f55c0985df7dbec
AlekseyPaymetov <fa_l@mail.ru>
2018-12-03 15:36:29 +0300

    Added solution for task 2.6 ring

---
+++ Task02-oop/Epam.Task02-oop/Epam.Task02-oop.6 ring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02_oop._6_ring
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double x = 0;
+            double y = 0;
+            double innerRadius = 6;
+            double outerRadius = 9;
+            Ring ring = new Ring(x, y, innerRadius, outerRadius);
+            Console.WriteLine($"Inicial ring with x={x}, y={y}, inner radius={innerRadius}, outer radius={outerRadius}");
+            Console.WriteLine("Area of the ring: {0:.000}", ring.Area);
+            Console.WriteLine("Total length of the ring: {0:.000}", ring.TotalLength);
+        }
+    }
+}
---
+++ Task02-oop/Epam.Task02-oop/Epam.Task02-oop.6 ring/Ring.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Epam.Task02_oop._1_Round;
+
+namespace Epam.Task02_oop._6_ring
+{
+    public class Ring
+    {
+        private Round innerRound;
+        private Round outerRound;
+
+        public Ring(double x, double y, double innerRadius, double outerRadius)
+        {
+            this.CheckInnerAndOuterRadius(innerRadius, outerRadius);
+            this.innerRound = new Round(x, y, innerRadius);
+            this.outerRound = new Round(x, y, outerRadius);
+        }
+
+        public double Area
+        {
+            get => this.outerRound.Area - this.innerRound.Area;
+        }
+
+        public double TotalLength
+        {
+            get => this.outerRound.Length + this.innerRound.Length;
+        }
+
+        protected void CheckInnerAndOuterRadius(double innerRadius, double outerRadius)
+        {
+            if (innerRadius >= outerRadius)
+            {
+                throw new ArgumentException("Inner radius should be smaller then outer radius");
+            }
+        }
+    }
+}
