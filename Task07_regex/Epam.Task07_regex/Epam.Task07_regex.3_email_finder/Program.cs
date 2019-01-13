07f63bfcfdab38104fb39e3194f5838fed132c8b
AlekseyPaymetov <fa_l@mail.ru>
2019-01-13 09:57:43 +0300

    Added solution for task 7.3 email.finder

---
+++ Task07_regex/Epam.Task07_regex/Epam.Task07_regex.3_email_finder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Epam.Task07_regex.WorkWithRegex;
+
+namespace Epam.Task07_regex._3_email_finder
+{
+    public class Program
+    {
+        private static string reg = @"\w[\w\.-_]+@[-\w+\.]+\.[A-zА-яЁё]{2,6}";
+
+        public static void Main(string[] args)
+        {
+            WorkWithRegexInConsole workWithEmailRegex = new WorkWithRegexInConsole(3, reg);
+            MatchCollection matches = workWithEmailRegex.GetAllMatches();
+            Console.WriteLine("Found email addresses:");
+            foreach (var item in matches)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"For example:{Environment.NewLine}{workWithEmailRegex.ExampleString}{Environment.NewLine}Found email addresses:");
+            matches = workWithEmailRegex.GetAllMatchesInExample();
+            foreach (var item in matches)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
