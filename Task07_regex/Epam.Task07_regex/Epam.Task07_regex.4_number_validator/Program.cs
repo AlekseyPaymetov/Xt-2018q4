e3dc0c060c115192b767aa05a1c61cb2b0ce4015
AlekseyPaymetov <fa_l@mail.ru>
2019-01-13 09:07:06 +0300

    Added solution for task 7.4 number validator.

---
+++ Task07_regex/Epam.Task07_regex/Epam.Task07_regex.4_number_validator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Epam.Task07_regex.WorkWithRegex;
+
+namespace Epam.Task07_regex._4_number_validator
+{
+    public class Program
+    {
+        private static string regForNumber = @"^[-+]?\d+(\.?\d+)?$";
+        private static string regForENumber = @"^[-+]?\d\.\d+[eE][+-]?\d+$";
+
+        public static void Main(string[] args)
+        {
+            WorkWithRegexInConsole findNumberInRegex = new WorkWithRegexInConsole(4, regForNumber);
+
+            if (findNumberInRegex.GetAllMatches().Count > 0)
+            {
+                Console.WriteLine("Input string is a number in ordinary notation");
+                return;
+            }
+
+            WorkWithRegexInConsole findENumberInRegex = new WorkWithRegexInConsole(4, regForENumber, false);
+            findENumberInRegex.InputString = findNumberInRegex.InputString;
+            if (findENumberInRegex.GetAllMatches().Count > 0)
+            {
+                Console.WriteLine("Input string is a number in scientific notation");
+                return;
+            }
+
+            Console.WriteLine("Input string is not a number.");
+        }
+    }
+}
